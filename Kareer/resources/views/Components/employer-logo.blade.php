1a8038b5596f79d0e54f143ea1632cc382ab29b9
Kongkham LUANGKHOT <kongkham.luangkhot@gmail.com>
2025-07-07 00:12:54 -0700

    styling broken image display

--- Kareer/resources/views/Components/employer-logo.blade.php
+++ Kareer/resources/views/Components/employer-logo.blade.php
@@ -1,4 +1,13 @@
 @props(['employer','size'])
 
 {{-- asset() help prepare url nicely --}}
-<img class="rounded-xl" src="{{ asset("$employer->logo") }}" alt="logo" width="{{ $size }}">
+<img
+    class="rounded-xl border border-neutral-800 bg-neutral-800/20"
+    src="{{ asset("$employer->logo") }}"
+    alt="logo"
+    onerror="this.style='padding:30%; text-align:end; align-content:center;'"
+    width="{{ $size }}"
+    height="{{ $size }}"
+>
+
+
