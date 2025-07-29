701ce2bd5aaa0d5e769bcc34a4146a02cc9050f3
Kongkham LUANGKHOT <kongkham.luangkhot@gmail.com>
2025-07-29 02:07:36 -0700

    log out response from ping app

--- Kareer/app/Http/Controllers/HttpRequestController.php
+++ Kareer/app/Http/Controllers/HttpRequestController.php
@@ -25,12 +25,19 @@ class HttpRequestController extends Controller
         Log::info("Ping received from: $sender");
 
         // Send request back in background (non-blocking)
-        Http::retry(3, 500) // retry 3 times, 1s interval
+        $response = Http::retry(3, 500) // retry 3 times, 1s interval
             ->timeout(5)
             ->withoutVerifying()
             ->withHeaders([ 'X-Sender-App' => 'KAREER App' ])
             ->get($targetURL);
 
+        if ($response->successful()) {
+            $body = $response->body(); // This should be "Ping Received!"
+            Log::info("$body");
+        } else {
+            Log::error("Ping failed with status: " . $response->status());
+        }
+        
         Log::info("Ping back: $targetURL");
         return response()->json(['message' => 'Ping received', 'sender' => $sender]);
     }
